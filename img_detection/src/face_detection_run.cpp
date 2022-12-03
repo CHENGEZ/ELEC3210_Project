727b7912f23307f6ed7dbcec0e3479ebfcf945b0
CHENGEZ <1428624697@qq.com>
2022-12-03 20:05:05 +0800

    classify whole input img instead of just the face

--- img_detection/src/face_detection_run.cpp
+++ img_detection/src/face_detection_run.cpp
@@ -16,26 +16,37 @@ ros::Publisher arrow_marker_publisher;
 Ptr<face::FaceRecognizer> model;
 CascadeClassifier faceDetection;
 const string MODEL_PATH = "/home/cyz/catkin_ws/src/img_detection/model.xml";
+const string WINDOW_NAME = "Camera Image";
 
 void image_callback(const sensor_msgs::ImageConstPtr &msg)
 {
     // convert from sensor_msgs::Image to cv::Mat
-    cv_bridge::CvImagePtr ptr = cv_bridge::toCvCopy(msg,sensor_msgs::image_encodings::BGR8);
+    cv_bridge::CvImagePtr ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
     Mat img = ptr->image;
-    
+    Mat imgcpy = img;
+
     // first try to extract out the face
     std::vector<Rect> faces;
-    faceDetection.detectMultiScale(img,faces);
+    faceDetection.detectMultiScale(img, faces);
 
-    // if there is face extracted, try to classify the extracted face
+    // show the extracted face in an opencv window
+    for (int i = 0; i < faces.size(); ++i)
+    {
+        rectangle(imgcpy, faces[i].tl(), faces[i].br(), Scalar(50,50,255), 3);
+    }
+    imshow(WINDOW_NAME, imgcpy);
+    waitKey(1);
+
     if (faces.size() >= 1)
     {
+        // if there is face extracted, try to classify the face
         cvtColor(img, img, COLOR_BGR2GRAY);
         Mat face = img(faces[0]);
-        ROS_INFO_STREAM(faces.size()<< " faces are detected");
-        string img_person_name;
+        ROS_INFO_STREAM(faces.size() << " face(s) is/are detected");
 
-        switch (model->predict(face))
+        string img_person_name;
+        // switch (model->predict(face))
+        switch (model->predict(img))
         {
         case 0:
             img_person_name = "cartoon";
@@ -58,10 +69,12 @@ void image_callback(const sensor_msgs::ImageConstPtr &msg)
 
         ROS_INFO_STREAM(img_person_name << " detected in the input image!");
 
+        // add marker
+        
     }
     else
     {
-        // clear any markers on screen
+        // if there are no faces extracted, clear any markers on screen
         ROS_INFO_STREAM("No face is detected in the input image.");
     }
 }
@@ -74,12 +87,14 @@ int main(int argc, char **argv)
     model = face::LBPHFaceRecognizer::create();
     model->read(MODEL_PATH);
 
-    if(!faceDetection.load("/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml"))
+    if (!faceDetection.load("/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml"))
     {
         ROS_INFO_STREAM("Failed to load face detector. Check environment setup.");
         exit(-1);
     }
 
+    namedWindow(WINDOW_NAME);
+    
     arrow_marker_publisher = face_detection_node_handle.advertise<visualization_msgs::Marker>("visualization_marker", 1);
     img_subcriber = face_detection_node_handle.subscribe("/vrep/image", 1, image_callback);
 
